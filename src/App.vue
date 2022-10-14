b58bb05d259e57ff2a40d1d1f1b040a69329ea55
Annie Ehler <annie.ehler.4@gmail.com>
2022-10-14 09:54:17 -0700

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="tile"></div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/home.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    home,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/home.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="tiles">
+    <div class="tile" @resize="createGrid()"></div>
+  </div>
+</template>
+
+<script>
+import { isExpressionWrapper } from '@babel/types';
+
+export default {
+  name: "home",
+  props: {
+    methods: {
+      createGrid() {
+        const createTile = index => {
+          const tile = document.createElement("div");
+          tile.classList.add("tile");
+          return tile;
+        }
+
+        const createTiles = quantity => {
+          Array.from(Array(quantity)).map((tile, index) => {
+            wrapper.appendChild(createTile(index));
+          })
+        }
+
+        wrapper.innerHTML = "";
+
+        let columns = Math.floor(document.body.clientWidth / 50);
+        let rows = Math.floor(document.body.clientHeight / 50);
+
+        createTiles(columns * rows);
+      },
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  #tiles{
+    height: 100vh;
+    width: 100vh;
+
+    display: grid;
+  }
+
+  .tile {
+    outline: 1px solid white;
+  }
+</style>
